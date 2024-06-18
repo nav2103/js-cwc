2e2f78765364cd509904412a1c9b26b1998d554b
Navneet Singh <98378534+nav2103@users.noreply.github.com>
2024-06-18 03:27:20 +0000

    dataypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let name = "navneet" //string
+let age = 21 //number
+let isAvailable = true //boolean
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
