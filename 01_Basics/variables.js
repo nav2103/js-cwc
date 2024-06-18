74cd747294cd553a975a2b8037856ff6093aac1c
Navneet Singh <98378534+nav2103@users.noreply.github.com>
2024-06-18 03:02:46 +0000

    lecture-1 Variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "navneet@microsoft.com"
+var accountPassword = "navneet"
+accountCity = "Jeypore"
+let accountState;
+// accountId = 231 //not allowed to change a constant
+
+accountEmail = "navneet@google.com"
+accountPassword = "teenvan"
+accountCity = "Hyderabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
