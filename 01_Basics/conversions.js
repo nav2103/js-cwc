458d887b0031c1a2fb79c5a16a59624df88f52e7
Navneet Singh <98378534+nav2103@users.noreply.github.com>
2024-06-23 03:41:15 +0000

    conversions-added

---
+++ 01_Basics/conversions.js
@@ -0,0 +1,20 @@
+// let score = "33wer";
+// let score = null;
+// let score = undefined;
+let score = true;
+
+// console.log(typeof score); // string / object / undefined / boolean
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN / 0 / NaN / 1
+
+
+let isLoggedIn = 1;
+let boolILI = Boolean(isLoggedIn);
+// console.log(boolILI);
+
+let num = 12;
+let strNum = String(num);
+console.log(strNum);
