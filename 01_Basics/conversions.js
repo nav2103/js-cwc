a2a5bc55ad7e3b6cec437f5f879123f27b76c8bf
Navneet Singh <98378534+nav2103@users.noreply.github.com>
2024-06-23 04:55:07 +0000

    operations-added

--- 01_Basics/conversions.js
+++ 01_Basics/conversions.js
@@ -17,4 +17,22 @@ let boolILI = Boolean(isLoggedIn);
 
 let num = 12;
 let strNum = String(num);
-console.log(strNum);
+// console.log(strNum);
+
+/* *************************************Operations************************************************* */
+
+let str1 = "Hey, "
+let str2 =  "Navneet"
+console.log(str1+str2);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log(1 + 2 + "2"); //32
+console.log("1" + 2 + 2); //122
+
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 5
+console.log(num1,num2,num3);
+
