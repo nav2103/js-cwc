0a566ecdadc884f692071e6312d2b3f2280e6181
Navneet Singh <98378534+nav2103@users.noreply.github.com>
2024-06-26 04:48:24 +0000

    comparisions-added

---
+++ 01_Basics/comparisions.js
@@ -0,0 +1,10 @@
+// console.log("2" < 1);       //false
+// console.log("02" < 1);      //false
+
+// console.log(null == 0);     //false
+// console.log(null > 0);      //false
+// console.log(null >= 0);     //true
+
+
+console.log("2" == 2);      //true
+console.log("2" === 2);     //false (Because strict check checks the datatype as well.)
